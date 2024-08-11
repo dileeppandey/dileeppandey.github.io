50d6e16ad22f7dd90861bd4526c961eb09db8751
Dileep Pandey <pandey.dileep.in@gmail.com>
2024-08-11 12:51:08 -0500

    update insta link

--- components/layout.tsx
+++ components/layout.tsx
@@ -64,7 +64,7 @@ export default function Layout({
                   />
                 </a>
               </Link>
-              <Link legacyBehavior href="https://www.instagram.com/theleapnext/">
+              <Link legacyBehavior href="https://www.instagram.com/dileep.io/">
                 <a target='_blank'>
                   <Image
                     priority
